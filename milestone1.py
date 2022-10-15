38ab9d22cbe185a469eb76c2c38e5a4e6f564e78
ethanlh2 <115669400+ethanlh2@users.noreply.github.com>
2022-10-14 19:44:11 -0500

    Changed rna2codon

--- milestone1.py
+++ milestone1.py
@@ -183,8 +183,21 @@ def splice_rna(dna, intron_list):
     rna = dna2rna(dna)
     return rna2codon(rna)
 def rna2codon(triplets):
+    codon2amino = {
+            'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+            'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+
+            'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+            'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+
+            'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+            'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+
+            'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+            'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+        }
     amino = ''
     for triplet in range(0,int( len( triplets ) / 3 )):
         triplet = triplets[3*triplet:3*triplet+3]
-        amino = amino + rna2codon(triplet)
+        amino = amino + codon2amino[triplet]
     return amino
